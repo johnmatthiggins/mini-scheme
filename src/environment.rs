648e07af35e437d46c53dcd0ea1e2aeffcf0dff2
John M. Higgins <johnmatthiggins@gmail.com>
2022-01-09 16:21:50 -0800

    Added car cdr pattern for evaluating AST

--- src/environment.rs
+++ src/environment.rs
@@ -18,21 +18,51 @@ impl EnvTrait for Env {
         return self.get(symbol);
     }
 
-    fn eval(&mut self, input: &String) -> Result<String, String> {
+    fn eval(&mut self, input: &String) -> String {
         let tokens = lexer::lexical_analysis(input);
+        let ast = lexer::parse_tokens(&tokens);
 
-        let result = match tokens {
-            Ok(vec) => print_tree(&lexer::parse_tokens(&vec)),
-            Err(err) => err
-        };
+        let result = self.simplify(&ast);
 
         return result;
     }
 
-    fn simplify(&self, expr: Expr) -> Expr {
+    fn eval_list(list: &Vec<Expr>) -> Result<Expr, String> {
+        // Evaluate list expression.
+        let mut simplified_list:
+            Vec<Result<Expr, String>> = Vec::new();
+
+        let car = list.first();
+
+        match car {
+            Some(expr) => match expr {
+                Expr::Atom(atom)
+                    => eval_car_cdr(atom, list[1..].to_vec()),
+                Expr::List(list)
+                    => Result::Err("First token in list must be function name.")
+            }
+            None => Result::Err("Empty list is not a valid token.")
+        }
+    }
+
+    fn eval_car_cdr(&mut self, car: Atom, cdr: &Vec<Expr>) -> Result<Expr, String> {
+        match car {
+            Atom::Symbol(name) => apply(car, cdr),
+            _ => Result::Err("First token in list must be function name.")
+        }
+    }
+
+    fn apply(&mut self, name: &String, args: &Vec<Expr>) -> Result<Expr, String> {
+        // Match functions to their name and return a function not found error
+        // if it doesn't exist in the environment or in built in functions.
+        return Result::Err("Good job! You reached the end of the level!");
+    }
+
+    fn simplify(&mut self, expr: &Expr) -> Result<Expr, String> {
         match expr {
-            Expr::Atom(atom) => Expr::Atom(&atom),
-            Expr::List(list) => // figure out what to do now.
+            Expr::List(list) => eval_list(self, list),
+            // Don't worry about atoms right now.
+            Expr::Atom(atom) => Result::Ok(Expr::Atom(atom))
         }
     }
 }
--- src/ops.rs
+++ src/ops.rs
@@ -1,129 +1,60 @@
 use bigdecimal::BigDecimal;
 
 mod ops {
-    const ARG_COUNT_ERROR: str = "Incorrect number of arguments for '{}'";
-
+    // Return first element of list or just empty.
     pub fn car(expr: &Expr) -> Result<Expr, String> {
         match expr {
-            Expr::Atom(a) => expr,
-            Expr::List(list) => car_is_list(&list),
+            Expr::Atom(atom) => Result::Err("'car' can only be applied to lists."),
+            Expr::List(list) => values.first() match {
+                Some(atom) => Result::Ok(atom),
+                None => Result::Err("'car' cannot be applied to empty lists.")
+            }
         }
-    }
-
-    fn car_is_list(list: &Vec<Expr>) -> Expr {
-        values[0];
     }
 
     pub fn cdr(expr: &Expr) -> Expr {
         match expr {
-            Expr::Atom(a) => expr,
-            Expr::List(list) => cdr_is_list(list)
+            Expr::Atom(a) => Result::Err("'cdr' can only be applied to lists."),
+            Expr::List(list) => Expr::List(list[1..].to_vec())
         }
     }
 
-    pub fn cdr_is_list(list: &Vec<Expr>) -> Expr {
-        Expr::List(list[1..]);
-    }
+    pub fn eq(cdr: &[Expr]) -> Result<Expr, String> {
+        let first = cdr.first();
 
-    pub fn eq(values: &Vec<LValue>) -> Result<LValue, String> {
-        if values.len() != 2 {
-            Result::Err(format!(ARG_COUNT_ERROR, "="));
-        }
-        else {
-            // If not the same type return false.
-            match (values[0], values[1]) {
-                (Atom::Number(a), Atom::Number(b)) => Result::Ok(Atom::Bool(a != b)),
-                (Atom::Bool(a), Atom::Bool(b)) => Result::Ok(Atom::Bool(a != b)),
-                (Atom::String(s1), Atom::String(s2)) => Result::Ok(Atom::Bool(s1.ne(s2)),
-                // TODO: Write case for list equality.
-                (Atom::List(v1), Atom::List(v2)) => {
-                    if v1.len() == v2.len() {
-                    }
-                    else {
-                        let result = Result::Ok(LValue::Bool(false));
-                    }
-
-                    return result;
-                },
-                (Atom::String(s), Atom::Number(n))
-                    => Result::Ok(Atom::Bool(n.to_string.ne(s))
-                _ => Result::Ok(Atom::Bool(false));
+        match first {
+            Some(s) => {
+                let mut is_eq = true;
+                
+                // Loop through all of them and make sure
+                // they are equal to each other.
+                for (i, exp) in cdr.iter().enumerate() {
+                    is_eq = is_eq && s == exp;
+                }
             }
+            None => Result::Err("Incorrect argument count for '=' operator."),
         }
-    }
-
-    pub fn eq(expr: Expr) -> Result<Expr, String> {
-        match expr {
-            Expr::Atom(atom) => Expr::Atom(Atom::Boolean(false)),
-            Expr::List(list) => eq_list(&list)
-        }
-    }
-
-    fn eq_list(list: &Vec<Expr>) -> Result<Expr, String> {
-    }
-
-    fn eq_two_args() -> {
     }
 
     // elementary functions of math.
-    
-    // Accumulator function to reduce code.
-    pub fn acc(values: &Vec<LValue>, op: fn(i64, i64) -> i64) -> Result<LValue, String> {
-        if values.len() > 2 {
-            let total: i64 = 0;
-            let length = values.len();
 
-            for (i, v) in values.enumerate() {
-                if let LValue::Number(n) = v {
-                    total = op(total, n);
-
-                    if values.len() == i + 1 {
-                        let result = Result::Ok(total);
-                    }
-                }
-                else {
-                    let result = Result::Err("Cannot perform operation on non numeric type.");
-                    break;
-                }
-            }
-        }
-        else {
-            let result = Result::Err("Cannot perform operation on fewer than two operands.");
-        }
-    }
-
-    fn add(expr: Expr) -> Result<Expr, String> {
-        match expr {
-            Expr::Atom(atom) => ,
-            Expr::List(list) => add_list(&list)
-        }
-    }
-
-    fn number_or_error(atom: Atom) -> Result<Expr, String> {
-        match atom {
-            Atom::Number(n) => Result::Ok(Expr::Atom(Atom::Number(n))),
-            _ => Result::Err("Mathematical operators can only be applied to numbers.")
-        }
-    }
-
-    fn add_list(list: &Vec<Expr>) -> Result<Expr, String> {
+    fn add(args: &Vec<Expr>) -> Result<Expr, String> {
         let total: BigDecimal = 0;
         let length = list.len();
         
         for (i, expr) in list.enumerate() {
-             
         }
     }
 
-    pub fn sub_list(list: &Vec<Expr>) -> Result<Expr, String> {
+    pub fn sub_list(args: &Vec<Expr>) {
     }
     
-    pub fn mul_list(list: &Vec<Expr>) -> Result<Expr, String> {
+    pub fn mul_list(list: &Vec<Expr>) {
     }
 
-    pub fn div_list(list: &Vec<Expr>) -> Result<Expr, String> {
+    pub fn div_list(list: &Vec<Expr>) {
     }
 
-    pub fn mod_list(list: &Vec<Expr>) -> Result<Expr, String> {
+    pub fn mod_list(list: &Vec<Expr>) {
     }
 }
