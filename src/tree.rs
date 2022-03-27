4c51676d33306c852ea46fd5407bb3d867a23e0b
John Higgins <johnmatthiggins@gmail.com>
2022-03-27 13:16:23 -0700

    Trying to write code for moving up tree.

--- src/tree.rs
+++ src/tree.rs
@@ -38,11 +38,20 @@ pub fn init_traversal(root: Expr) -> Traversal {
 }
 
 pub trait TraversalOps {
+    fn find_next(&mut self);
     pub fn run(&mut self) -> Result<Expr, String>;
 }
 
 // Primary evaluation algorithm for interpreter.
 impl Traversal for TraversalOps {
+    fn find_next(&mut self) {
+        loop {
+            // Stackframe should have new Expr added to it from current iteration.
+            // Path should be one less element because we are going to search for siblings.
+            // 
+        }
+    }
+
     fn run(&mut self) -> Result<Expr, String> { 
         // Loop until we get the ExitLoop command.
         loop {
@@ -65,46 +74,43 @@ impl Traversal for TraversalOps {
                     // Then put result onto stack and move to upper node.
                     // Exit loop if stack is empty.
                 let sibling_count = get_parent(current)
-                        .and_then(|x| (u32)x.len())
+                        .and_then(|x| match x {
+                            Expr::List(l) => (u32)l.len(),
+                            _ => None
+                        })
                         .or(0);
 
-                let curr_frame = frames.last().unwrap();
+                let curr_frame = self.call_stack.last_mut().unwrap();
                 let curr_arg_count = curr_frame.args.len();
             
                 if sibling_count == curr_arg_count {
-                    result = exec_stackframe(curr_frame);
+                    // Execute stack frame once we have enough arguments in it.
+                    let exec_frame = self.call_stack.args.pop();
+                    result = exec_stackframe(exec_frame).unwrap();
 
-                    // Add result to stackframe if it resulted in Ok()
-                    // Then move up to parent nodes until a sibling exists.
-                
-                    // grab path excluding last turn.
-                    let rest_of_path = path.split_last()
-                        .and_then(|x| x.1)
-                        .or(Vec::new());
+                    match result {
+                        Ok(v) => {
+                            self.call_stack.args.push(v);
                     
-                    // If the list isn't long enough, just exit and return our result. 
-                    match rest_of_path {
-                        Some(v) => {
-                            path = v.to_vec();
+                            // Seek next node in tree.
+                            self.find_next();
                         },
-                        None => {
-                            exit = true;
+                        Err(err) => {
+                            // Show error and exit loop.
+                            result = Result::Err(err.to_owned());
+                            break;
                         }
                     }
                 }
+                // If stack args aren't full, move directly to sibling node.
                 else {
                     // Add one to it to get next sibling.
                     let mut last = sibling_path.last_mut();
                     last += 1;
                     
-                    // because current is leaf, we can push onto arg stack.
-                    self.current = self.call_stack.args.last().unwrap().push(self.current);
-                
                     // then we assign current as next child in line.
                     self.current = get_child(self.root, self.path);
                 }
-
-                // If stack args aren't full move to sibling node.
             }
             else {
                 // Move down.
